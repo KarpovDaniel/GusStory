e3b7432659abd3f20a035fbfbfa6ea424a06cfd5
Daniel <karpovdaniel2003@gmail.com>
2020-11-24 17:14:06 +0300

    geo 3

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,4 +1,5 @@
 ymaps.ready(init, function () {
+    var map;
     ymaps.geolocation.get().then(function (res) {
         var mapContainer = $('#map'),
             bounds = res.geoObjects.get(0).properties.get('boundedBy'),
@@ -7,7 +8,16 @@ ymaps.ready(init, function () {
                 [mapContainer.width(), mapContainer.height()]
             );
         createMap(mapState);
+    }, function (e) {
+        createMap({
+            center: [55.751574, 37.573856],
+            zoom: 2
+        });
     });
+
+    function createMap (state) {
+        map = new ymaps.Map('map', state);
+    }
 });
 
 function init() {
