b3d98dca69a33b4a782f55fc2689f86f4bca202e
Daniel <karpovdaniel2003@gmail.com>
2020-11-29 13:16:53 +0300

    метки

--- static/js/maps.js
+++ static/js/maps.js
@@ -6,176 +6,116 @@ function init() {
         zoom: 14
     });
 
-    function constructor(coord, content, content_h, content_b) {
-        const myGeoObject = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: coord
-            },
-            properties: {
+    function constructor(coord, content, content_header, content_body, icon) {
+        const myGeoObject = new ymaps.Placemark(
+            coord, {
                 hintContent: content,
-                balloonContentHeader: content_h,
-                balloonContentBody: content_b
-            }
-        }, {
-            preset: "islands#blueLeisureCircleIcon"
-        });
+                balloonContentHeader: content_header,
+                balloonContentBody: content_body
+            }, {preset: icon});
         myMap.geoObjects.add(myGeoObject);
     }
 
     constructor([55.619820, 40.658117],
         "Музей Хрусталя им. Мальцовых",
         "<a href='about_item/1' style='color:#000'>Музей Хрусталя им. Мальцовых</a>",
-        "ул. Калинина, 2А");
+        "ул. Калинина, 2А",
+        "islands#blueLeisureIcon");
     constructor([55.613656, 40.671539],
         "Церковь Иоакима и Анны",
         "<a href='about_item/18' style='color:#000'>Церковь Иоакима и Анны</a>",
-        "ул. Люксембургская, 3");
+        "ул. Люксембургская, 3",
+        "islands#blueChristianIcon");
     constructor([55.615304, 40.663793],
         "Гусевской хрустальный завод",
         "<a href='about_item/10' style='color:#000'>Гусевской хрустальный завод</a>",
-        "ул. Калинина, д.28");
+        "ул. Калинина, д.28",
+        "islands#blueFactoryIcon");
     constructor([55.618697, 40.650599],
         "Железнодорожная станция Гусь-Хрустальный",
         "<a href='about_item/11' style='color:#000'>Железнодорожная станция Гусь-Хрустальный</a>",
-        "ул. Вокзальная ул., 1");
+        "ул. Вокзальная ул., 1",
+        "islands#blueRailwayIcon");
     constructor([55.614639, 40.670134],
         "Торговые ряды",
         "<a href='about_item/14' style='color:#000'>Торговые ряды</a>",
-        "ул. Старых Большевиков, д.7");
+        "ул. Старых Большевиков, д.7",
+        "islands#blueMoneyIcon");
     constructor([55.618883, 40.659677],
         "Гусь-Хрустальный историко-художественный музей",
         "<a href='about_item/20' style='color:#000'>Гусь-Хрустальный историко-художественный музей</a>",
-        "ул. Калинина, 2");
+        "ул. Калинина, 2",
+        "islands#blueLeisureIcon");
     constructor([55.604197, 40.676754],
         "Часовня Св. Варвары",
         "<a href='about_item/5' style='color:#000'>Часовня Св. Варвары</a>",
-        "ул. Пролетарская улица");
+        "ул. Пролетарская улица",
+        "islands#blueChristianIcon");
     constructor([55.619020, 40.657013],
         "Богадельня",
         "<a href='about_item/6' style='color:#000'>Богадельня</a>",
-        "ул. Калинина, 1");
+        "ул. Калинина, 1",
+        "islands#blueChristianIcon");
     constructor([55.626131, 40.665206],
         "Городское озеро",
         "<a href='about_item/12' style='color:#000'>Городское озеро</a>",
-        "ул. Калинина");
+        "ул. Калинина",
+        "islands#blueWaterParkIcon");
     constructor([55.611645, 40.668197],
         "Гусевский текстильный комбинат",
         "<a href='about_item/9' style='color:#000'>Гусевский текстильный комбинат</a>",
-        "ул. Рудницкой, 6");
+        "ул. Рудницкой, 6",
+        "islands#blueFactoryIcon");
     constructor([55.616437, 40.678321],
         "Мальцовские дома",
         "<a href='about_item/8' style='color:#000'>Мальцовские дома</a>",
-        "ул. Октябрьская");
+        "ул. Октябрьская",
+        "islands#blueHomeIcon");
     constructor([55.619949, 40.678899],
         "Православная гимназия",
         "<a href='about_item/2' style='color:#000'>Православная гимназия</a>",
-        "Интернациональная ул., 52");
+        "Интернациональная ул., 52",
+        "islands#blueEducationIcon");
     constructor([55.611034, 40.658133],
         "Военный комиссариат города Гусь-Хрустальный и Гусь-Хрустального района Владимирской области",
         "<a href='about_item/16' style='color:#000'>Военный комиссариат города Гусь-Хрустальный и Гусь-Хрустального района Владимирской области</a>",
-        "ул. Васильева, 23");
+        "ул. Васильева, 23",
+        "islands#blueInfoIcon");
     constructor([55.616965, 40.677815],
         "Городская больница",
         "<a href='about_item/19' style='color:#000'>Городская больница</a>",
-        "Октябрьская ул., 39");
-    const myGeoObject2 = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [55.614354, 40.670724]
-            },
-            properties: {
-                hintContent: "Памятник Комсомольцам",
-                balloonContentHeader: "<a href='about_item/3' style='color:#000'>Памятник Комсомольцам</a>",
-                balloonContentBody: "площадь Свободы",
-            }
-        },
-        {
-            strokeColor: "ff0000",
-            preset: "islands#redLeisureCircleIcon"
-        });
-    const myGeoObject3 = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [55.617119, 40.664703]
-            },
-            properties: {
-                hintContent: "Узкоколейная железная дорога",
-                balloonContentHeader: "<a href='about_item/4' style='color:#000'>Узкоколейная железная дорога</a>",
-                balloonContentBody: "улица Калинина, 20",
-            }
-        },
-        {
-            strokeColor: "ff0000",
-            preset: "islands#redLeisureCircleIcon"
-        });
-    const myGeoObject5 = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [55.617810, 40.664254]
-            },
-            properties: {
-                hintContent: "Завод &laquo;Стекловолокно&raquo;",
-                balloonContentHeader: "<a href='about_item/7' style='color:#000'>Завод &laquo;Стекловолокно&raquo;</a>",
-                balloonContentBody: "улица Калинина, 9",
-            }
-        },
-        {
-            strokeColor: "ff0000",
-            preset: "islands#redLeisureCircleIcon"
-        });
-    const myGeoObject11 = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [55.621394, 40.643527]
-            },
-            properties: {
-                hintContent: "ДШИ",
-                balloonContentHeader: "<a href='about_item/13' style='color:#000'>ДШИ</a>",
-                balloonContentBody: "просп. 50 лет Советской Власти, 13",
-            }
-        },
-        {
-            strokeColor: "ff0000",
-            preset: "islands#redLeisureCircleIcon"
-        });
-    const myGeoObject12 = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [55.619496, 40.658481]
-            },
-            properties: {
-                hintContent: "Памятник Акиму Мальцову",
-                balloonContentHeader: "<a href='about_item/15' style='color:#000'>Памятник Акиму Мальцову</a>",
-                balloonContentBody: "микрорайон Вокзал",
-            }
-        },
-        {
-            strokeColor: "ff0000",
-            preset: "islands#redLeisureCircleIcon"
-        });
-    const myGeoObject13 = new ymaps.GeoObject({
-            geometry: {
-                type: "Point",
-                coordinates: [55.615298, 40.670767]
-            },
-            properties: {
-                hintContent: "Памятник Корсакову",
-                balloonContentHeader: "<a href='about_item/17' style='color:#000'>Памятник Корсакову</a>",
-                balloonContentBody: "микрорайон Центр",
-            }
-        },
-        {
-            strokeColor: "ff0000",
-            preset: "islands#redLeisureCircleIcon"
-        });
-    myMap.geoObjects.add(myGeoObject2);
-    myMap.geoObjects.add(myGeoObject3);
-    myMap.geoObjects.add(myGeoObject5);
-    myMap.geoObjects.add(myGeoObject11);
-    myMap.geoObjects.add(myGeoObject12);
-    myMap.geoObjects.add(myGeoObject13);
-
+        "Октябрьская ул., 39",
+        "islands#blueMedicalIcon");
+    constructor([55.614354, 40.670724],
+        "Памятник Комсомольцам",
+        "<a href='about_item/3' style='color:#000'>Памятник Комсомольцам</a>",
+        "площадь Свободы",
+        "islands#bluePersonIcon");
+    constructor([55.617119, 40.664703],
+        "Узкоколейная железная дорога",
+        "<a href='about_item/4' style='color:#000'>Узкоколейная железная дорога</a>",
+        "улица Калинина, 20",
+        "islands#blueRailwayIcon");
+    constructor([55.617810, 40.664254],
+        "Завод &laquo;Стекловолокно&raquo",
+        "<a href='about_item/7' style='color:#000'>Завод &laquo;Стекловолокно&raquo;</a>",
+        "улица Калинина, 9",
+        "islands#blueFactoryIcon");
+    constructor([55.621394, 40.643527],
+        "ДШИ",
+        "<a href='about_item/13' style='color:#000'>ДШИ</a>",
+        "просп. 50 лет Советской Власти, 13",
+        "islands#blueEducationIcon");
+    constructor([55.619496, 40.658481],
+        "Памятник Акиму Мальцову",
+        "<a href='about_item/15' style='color:#000'>Памятник Акиму Мальцову</a>",
+        "микрорайон Вокзал",
+        "islands#bluePersonIcon");
+    constructor([55.615298, 40.670767],
+        "Памятник Корсакову",
+        "<a href='about_item/17' style='color:#000'>Памятник Корсакову</a>",
+        "микрорайон Центр",
+        "islands#bluePersonIcon");
     const geolocation = ymaps.geolocation;
     geolocation.get({
         provider: 'browser'
