d9e3557e0c7766f17e4f365432dbcaa05ad9450e
maximzobkov <56Vf5mg1>
2020-12-01 21:10:47 +0300

    Так то лучше

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ from data import db_session, items, users, quests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'GusStory.ru'
-db_session.global_init("../db/blogs.sqlite")
+db_session.global_init("db/blogs.sqlite")
 login_manager = LoginManager()
 login_manager.init_app(app)
 count_items = 0
