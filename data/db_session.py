c8945c31a3b6b1cd08b73335f70fc58f0ae50d5a
Daniel <karpovdaniel2003@gmail.com>
2021-01-04 14:28:59 +0300

    update db_session

--- data/db_session.py
+++ data/db_session.py
@@ -1,28 +1,16 @@
-import sqlalchemy as sa
-import sqlalchemy.ext.declarative as dec
-import sqlalchemy.orm as orm
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
-SqlAlchemyBase = dec.declarative_base()
-
+SqlAlchemyBase = declarative_base()
 __factory = None
 
 
 def global_init(db_file):
     global __factory
-
-    if __factory:
-        return
-
-    if not db_file or not db_file.strip():
-        raise Exception('Необходимо указать файл базы данных.')
-
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
-    print(f'Подключение к базе данных по адресу {conn_str}')
-
-    engine = sa.create_engine(conn_str, echo=False)
-    __factory = orm.sessionmaker(bind=engine)
-
+    engine = create_engine(f'sqlite:///{db_file.strip()}?check_same_thread=False', echo=False)
+    __factory = sessionmaker(bind=engine)
     SqlAlchemyBase.metadata.create_all(engine)
 
 
